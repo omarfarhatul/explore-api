af80b2aa116c385d4ba24e4e1072464cd822089e
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-17 19:37:13 +0600

    Add to data using fetch

---
+++ js/posts.js
@@ -0,0 +1,21 @@
+function loadPosts(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(data => displayPosts(data))
+}
+
+function displayPosts(posts){
+    const postscontainer=document.getElementById('posts-container');
+    for(const post of posts){
+        const postdiv=document.createElement('div');
+        postdiv.classList.add('post');
+        postdiv.innerHTML=`
+        <h4>User-${post.userId}</h5>
+        <h5>Post: title: ${post.title}</h5>
+        <p>Post Description: ${post.body}</p>
+        `;
+        postscontainer.appendChild(postdiv);
+    }
+}
+
+loadPosts();
