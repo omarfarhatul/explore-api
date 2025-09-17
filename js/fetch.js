607f0ac8f0d61a7688bd32b7207a61634cc93c3b
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-17 18:56:12 +0600

    update

--- js/fetch.js
+++ js/fetch.js
@@ -9,7 +9,7 @@ fetch('https://jsonplaceholder.typicode.com/todos/1')
 // .then(json => console.log(json))
 
 function loadData(){
-    const url = 'https://jsonplaceholder.typicode.com/todos/1';
+    const url = 'https://jsonplaceholder.typicode.com/todos/1'
     fetch(url)
     .then(response => response.json())
     .then(json => console.log(json))
