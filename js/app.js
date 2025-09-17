206e07e74b3b362da9c98bfcf93042e1d62ad031
omarfarhatul <omarfarhatulhossain@gmail.com>
2025-09-17 19:06:00 +0600

    fetch

--- js/app.js
+++ js/app.js
@@ -3,3 +3,13 @@ function loadData2(){
     .then(res => res.json())
     .then(data => console.log(data))
 }
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => displayUsers(data))
+}
+
+function displayUsers(data){
+    console.log(data);
+}
